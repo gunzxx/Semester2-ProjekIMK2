2e894d0f7356101aa162a0a5a349933c04029700
gunzxx <ggm5850@gmail.com>
2022-06-17 12:40:24 +0700

    a

--- js/main.js
+++ js/main.js
@@ -1,36 +1,5 @@
-$(document).ready(function(){
-	$(".header-block-logo").click(function(){
-		//$(".header-block-item-layanan").hide();
-		window.location.href="MainPage.html";
- });
-	$(".header-block-item-layanan").click(function(){
-		//$(".header-block-item-layanan").hide();
-		window.location.href="Layanan.html";
- });
-
-	$(".header-block-item-konsultasi").click(function(){
-		//$(".header-block-item-layanan").hide();
-		window.location.href="PilihPsikolog.html";
- });
-	$(".header-block-item-testimoni").click(function(){
-		//$(".header-block-item-layanan").hide();
-		window.location.href="MainPage.html#testimoni-nih";
-		//$('body, html').animate({
-			//scrollTop: $(".main-page-block34 ").offset().top }, 600);
- });
-	$(".header-block-item-kerjasama").click(function(){
-		//$(".header-block-item-layanan").hide();
-		window.location.href="MainPage.html#contact-nih";
- });
-	$(".header-block-item-tentang").click(function(){
-		//$(".header-block-item-layanan").hide();
-		window.location.href="MainPage.html#tentang-nih";
- });
-	$(".header-profil-icon").click(function(){
-		//$(".header-block-item-layanan").hide();
-		window.location.href="userProfile.html";
- });
-	$(".main-page-block561.layout").click(function(){
+const daftar = document.querySelector(".main-page-block561.layout");
+daftar.addEventListener('click',(function(){
 		var keluar = confirm("Apakah anda ingin Logout?");
 		if (keluar == true){
 			window.location.href="Register.html";
@@ -38,5 +7,52 @@ $(document).ready(function(){
 		else{
 			return keluar;
 		}
-	});
-});
+	})
+)
+
+
+
+
+
+// $(document).ready(function(){
+// 	$(".header-block-logo").click(function(){
+// 		//$(".header-block-item-layanan").hide();
+// 		window.location.href="MainPage.html";
+//  });
+// 	$(".header-block-item-layanan").click(function(){
+// 		//$(".header-block-item-layanan").hide();
+// 		window.location.href="Layanan.html";
+//  });
+
+// 	$(".header-block-item-konsultasi").click(function(){
+// 		//$(".header-block-item-layanan").hide();
+// 		window.location.href="PilihPsikolog.html";
+//  });
+// 	$(".header-block-item-testimoni").click(function(){
+// 		//$(".header-block-item-layanan").hide();
+// 		window.location.href="MainPage.html#testimoni-nih";
+// 		//$('body, html').animate({
+// 			//scrollTop: $(".main-page-block34 ").offset().top }, 600);
+//  });
+// 	$(".header-block-item-kerjasama").click(function(){
+// 		//$(".header-block-item-layanan").hide();
+// 		window.location.href="MainPage.html#contact-nih";
+//  });
+// 	$(".header-block-item-tentang").click(function(){
+// 		//$(".header-block-item-layanan").hide();
+// 		window.location.href="MainPage.html#tentang-nih";
+//  });
+// 	$(".header-profil-icon").click(function(){
+// 		//$(".header-block-item-layanan").hide();
+// 		window.location.href="userProfile.html";
+//  });
+// 	$(".main-page-block561.layout").click(function(){
+// 		var keluar = confirm("Apakah anda ingin Logout?");
+// 		if (keluar == true){
+// 			window.location.href="Register.html";
+// 		}
+// 		else{
+// 			return keluar;
+// 		}
+// 	});
+// });
